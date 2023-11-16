5a49c12d1c618ba83064d7712a13600ae45aefe9
taka01150810 <taka01150810@gmail.com>
2023-11-16 20:56:49 +0900

    フォームにフォーカスしたらフォームが白色、フォーカスアウトしたらグレー色

--- my-trello/src/components/ListAdd.vue
+++ my-trello/src/components/ListAdd.vue
@@ -4,13 +4,15 @@
     @はv-onの省略記法。
     .preventはVサブミット時にリロードされるのを防ぐ。
 -->
-  <form class="addlist" @submit.prevent="addList">
+  <form :class="classList" @submit.prevent="addList">
     <!-- v-modelにtitleを定義することで、dataプロパティとバインド -->
     <input
       v-model="title"
       type="text"
       class="text-input"
       placeholder="リストを追加"
+      @focusin="startEditing"
+      @focusout="finishEditing"
     />
     <button type="submit" class="add-button">追加</button>
   </form>
@@ -20,7 +22,18 @@ export default {
   data: function () {
     return {
       title: "",
+      isEditing: false,
     };
+  },
+  // computedはdataの状態を監視できる
+  computed: {
+    classList() {
+      const classList = ["addlist"];
+      if (this.isEditing) {
+        classList.push("active");
+      }
+      return classList;
+    },
   },
   methods: {
     addList: function () {
@@ -28,6 +41,12 @@ export default {
       // 第二引数にオブジェクト型でdataプロパティのtitleを渡す
       this.$store.dispatch("addlist", { title: this.title });
       this.title = "";
+    },
+    startEditing: function () {
+      this.isEditing = true;
+    },
+    finishEditing: function () {
+      this.isEditing = false;
     },
   },
 };
