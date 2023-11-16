c24b703f6232587cfeebfa09893084036314d2fc
taka01150810 <taka01150810@gmail.com>
2023-11-16 21:16:10 +0900

    リストを表示しよう

--- my-trello/src/components/Board.vue
+++ my-trello/src/components/Board.vue
@@ -3,16 +3,42 @@
     <header>my Trello</header>
     <main>
       <p class="info-line">All: 0 tasks</p>
-      <list-add />
+      <div class="list-index">
+        <!-- 
+          Listコンポーネントに必要なデータを渡している 
+          「:」はv-bindの省略
+        -->
+        <list
+          v-for="(item, index) in lists"
+          :key="item.id"
+          :title="item.title"
+          :listIndex="index"
+        />
+        <list-add />
+      </div>
     </main>
   </div>
 </template>
 <script>
 import ListAdd from "./ListAdd.vue";
+import List from "./List";
+import { mapState } from "vuex";
 
 export default {
   components: {
     ListAdd,
+    List,
+  },
+  computed: {
+    /*
+    1. store/index.jsで定義したstateにthis.$store.stateでアクセスすることもできますが、
+    これを複数書いていくことを考えるとコードが助長になる。
+    そこで活躍するのがmapStateで、stateで定義されたデータの名前と同じ名前の文字列で
+    stateを呼び出すことができる。
+
+    2. computedにストアのstateからのデータと、ローカルでの算出データを両方定義する場合には、「...」を書く必要がある
+    */
+    ...mapState(["lists"]),
   },
 };
 </script>
