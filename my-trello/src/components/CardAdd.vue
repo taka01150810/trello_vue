00b9644f23cf587ba8fe1908f8ea483bc75be829
taka01150810 <taka01150810@gmail.com>
2023-11-16 21:46:14 +0900

    フォームがフォーカスされたらフォームを活性に、フォーカスアウトされたら非活性にしよう

--- my-trello/src/components/CardAdd.vue
+++ my-trello/src/components/CardAdd.vue
@@ -1,10 +1,12 @@
 <template>
-  <form class="addcard" @submit.prevent="addCardToList">
+  <form :class="classList" @submit.prevent="addCardToList">
     <input
       v-model="body"
       type="text"
       class="text-input"
       placeholder="カードを追加する"
+      @focusin="startEditing"
+      @focusout="finishEditing"
     />
     <button type="submit" class="add-button">追加</button>
   </form>
@@ -15,14 +17,31 @@ export default {
     listIndex: {
       type: Number,
       required: true,
+      isEditing: false,
     },
   },
   data: function () {
     return {
       body: "",
+      isEditing: false,
     };
   },
+  computed: {
+    classList() {
+      const classList = ["addcard"];
+      if (this.isEditing) {
+        classList.push("active");
+      }
+      return classList;
+    },
+  },
   methods: {
+    startEditing: function () {
+      this.isEditing = true;
+    },
+    finishEditing: function () {
+      this.isEditing = false;
+    },
     addCardToList: function () {
       // dispatchでactionsに定義したaddCardToListメソッドを実行
       this.$store.dispatch("addCardToList", {
